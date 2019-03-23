cb27bd4ce992dc60a593a0fa54447be292081cc6
Saad E <21321555+se447@users.noreply.github.com>
2019-03-23 15:12:21 -0400

    Set up java class

---
+++ src/PrintingLikeBoss.java
@@ -0,0 +1,27 @@
+public class PrintingLikeBoss {
+
+    public static void printStars(int amount) {
+    }
+
+    public static void printWhitespaces(int amount) {
+        // 40.1
+    }
+
+    public static void printTriangle(int size) {
+        // 40.2
+    }
+
+    public static void xmasTree(int height) {
+        // 40.3
+    }
+
+    public static void main(String[] args) {
+        // Tests do not use main, yo can write code here freely!
+
+        printTriangle(5);
+        System.out.println("---");
+        xmasTree(4);
+        System.out.println("---");
+        xmasTree(10);
+    }
+}
